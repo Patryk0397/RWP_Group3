87e8b3959627940018ba05ae434fd408b5fb1fbc
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2017-03-01 14:54:39 +0000

    added whole project from pc

---
+++ Gompare/app/src/main/java/com/example/sorin/gompare/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.sorin.gompare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    /* Duration of wait (miliseconds) */
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+         /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this,LoginActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
