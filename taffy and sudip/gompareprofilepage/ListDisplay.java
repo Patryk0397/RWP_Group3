d6e5fd7f9576edb7760a5058508d44f4acf049ac
TaffyChik <chikandt@coventry.ac.uk>
2017-03-10 14:10:25 +0000

    Add files via upload
    Taffy and Sudip

---
+++ taffy and sudip/gompareprofilepage/ListDisplay.java
@@ -0,0 +1,16 @@
+package com.example.gompare.gompareprofilepage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Admin on 08/03/2017.
+ */
+
+public class ListDisplay extends Activity{
+
+
+}
---
+++ taffy and sudip/gompareprofilepage/RoundImage.java
@@ -0,0 +1,20 @@
+package com.example.gompare.gompareprofilepage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Admin on 08/03/2017.
+ */
+
+public class RoundImage {
+
+}
