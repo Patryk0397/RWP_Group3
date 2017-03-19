ba1a6d1aa0021f1f86ed6ecbfd5e6ea659c10287
Patryk Orsztynowicz <patrykorsztynowicz1997@gmail.com>
2017-03-19 14:32:11 +0000

    logging in system is fully functioning

--- GompareV1/app/src/main/java/com/example/gompare/gomparev1/BackgroundTask.java
+++ GompareV1/app/src/main/java/com/example/gompare/gomparev1/BackgroundTask.java
@@ -1,12 +1,15 @@
 package com.example.gompare.gomparev1;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -19,6 +22,7 @@ import java.net.URLEncoder;
  */
 
 public class BackgroundTask extends AsyncTask<String,Void,String> {
+    AlertDialog alertDialog;
     Context ctx;
     BackgroundTask(Context ctx){
         this.ctx = ctx;
@@ -26,12 +30,13 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
 
     @Override
     protected void onPreExecute(){
-        super.onPreExecute();
+        alertDialog = new AlertDialog.Builder(ctx).create();
+        alertDialog.setTitle("Login Information...");
     }
     @Override
     protected String doInBackground(String...params){
         String reg_url = "http://192.168.1.112/register.php";
-        String login_url = "http://10.0.2.2/webapp/register.php";
+        String login_url = "http://192.168.1.112/login.php";
         String method = params[0];
 
         if (method.equals("register")){
@@ -67,6 +72,49 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
 
         }
 
+        else if(method.equals("login")){
+
+            String loginName = params[1];
+            String loginPass = params[2];
+
+            try {
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            String data = URLEncoder.encode("loginName", "UTF-8")+"="+URLEncoder.encode(loginName, "UTF-8")+"&"+
+                    URLEncoder.encode("loginPass", "UTF-8")+"="+URLEncoder.encode(loginPass, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String response = "";
+                String line = "";
+                while ((line = bufferedReader.readLine())!=null){
+
+                    response += line;
+
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return response;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
         return null;
     }
     @Override
@@ -75,7 +123,21 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
     }
     @Override
     protected void onPostExecute(String result){
-        Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+
+        if(result.equals("Registration Successful")){
+
+            Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+
+        }
+        else if(result.equals("Login Successful")) {
+
+            Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+        }
+        else{
+            alertDialog.setMessage(result);
+            alertDialog.show();
+        }
+
     }
 
 }
--- GompareV1/app/src/main/java/com/example/gompare/gomparev1/LoginPage.java
+++ GompareV1/app/src/main/java/com/example/gompare/gomparev1/LoginPage.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
+
+    EditText ETname, ETpass;
+    String loginName, loginPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,9 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.activity_login_page);
 
         TextView RegisterLink = (TextView) findViewById(R.id.RegisterLink);
+
+        ETname = (EditText) findViewById(R.id.loginUsername);
+        ETpass = (EditText) findViewById(R.id.loginPassword);
 
         RegisterLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +30,21 @@ public class LoginPage extends AppCompatActivity {
                 LoginPage.this.startActivity(RegisterLink);
             }
         });
+
     }
+
+    public void userLogin(View view){
+
+        loginName = ETname.getText().toString();
+        loginPass = ETpass.getText().toString();
+
+        String method = "login";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method, loginName,loginPass);
+
+
+
+    }
+
+
 }
